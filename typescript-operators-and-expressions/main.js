ff13cda90d543ea78b74c72a75913d33c44e01f2
Devin Olesen <devintanielu@gmail.com>
2024-07-10 03:08:06 +0000

    complete exercise

--- typescript-operators-and-expressions/main.js
+++ typescript-operators-and-expressions/main.js
@@ -1,39 +1,34 @@
-const width: number = 5;
-const height: number = 7;
-const area: number = width * height;
+'use strict';
+let width = 5;
+let height = 7;
+const area = width * height;
 console.log('value of area:', area);
 console.log('typeof area:', typeof area);
-
-const bill: number = 20;
-const payment: number = 50;
-const change: number = payment - bill;
+let bill = 20;
+let payment = 50;
+const change = payment - bill;
 console.log('value of change:', change);
 console.log('typeof change:', typeof change);
-
-const quizzes: number = 84;
-const midterm: number = 75;
-const final: number = 87;
-const grade: number = (quizzes + midterm + final) / 3;
+let quizzes = 84;
+let midterm = 75;
+let final = 87;
+const grade = (quizzes + midterm + final) / 3;
 console.log('value of grade:', grade);
 console.log('typeof grade:', typeof grade);
-
-const firstName: string = 'Devin';
-const lastName: string = 'Olesen';
-const fullName: string = firstName + ' ' + lastName;
+let firstName = 'Devin';
+let lastName = 'Olesen';
+const fullName = firstName + ' ' + lastName;
 console.log('value of fullName:', fullName);
 console.log('typeof fullName:', typeof fullName);
-
-const pH: number = 9;
-const isAcidic: boolean = pH < 7;
+let pH = 9;
+const isAcidic = pH < 7;
 console.log('value of isAcidic:', isAcidic);
 console.log('typeof isAcidic:', typeof isAcidic);
-
-const headCount: number = 277;
-const isSparta: boolean = headCount === 300;
+let headCount = 277;
+const isSparta = headCount === 300;
 console.log('value of isSparta:', isSparta);
 console.log('typeof isSparta:', typeof isSparta);
-
-let motto: string = fullName;
+let motto = fullName;
 motto += ' is the GOAT';
 console.log('value of motto:', motto);
 console.log('typeof motto:', typeof motto);
