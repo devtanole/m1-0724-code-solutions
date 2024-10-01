818c84471c99f02d700e5457fc499efd50b8930c
Devin Olesen <devintanielu@gmail.com>
2024-10-01 02:36:26 +0000

    fin

---
+++ practice-interview-challenge/main.ts
@@ -0,0 +1,28 @@
+function getRangeReport(start: number, end: number): any {
+  const avg: number = (start + end) / 2;
+  const rangeArr: number[] = [];
+  for (let i = start; i <= end; i++) {
+    rangeArr.push(i);
+  }
+  const evenArr: number[] = [];
+  for (let i = 2; i <= end; i += 2) {
+    evenArr.push(i);
+  }
+  const oddArr: number[] = [];
+  for (let i = 1; i <= end; i += 2) {
+    oddArr.push(i);
+  }
+  let total: number = 0;
+  for (let i = 0; i < rangeArr.length; i++) {
+    total += rangeArr[i];
+  }
+  return {
+    average: avg,
+    evens: evenArr,
+    odds: oddArr,
+    range: rangeArr,
+    sum: total,
+  };
+}
+
+console.log('getRange of 4 and 12:', getRangeReport(4, 12));
