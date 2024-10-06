b9fd7329749f486e414fe1b8ec7a90553ca38000
Devin Olesen <devintanielu@gmail.com>
2024-10-06 09:12:20 +0000

    quizz

---
+++ typescript-operators/main.js
@@ -0,0 +1,12 @@
+'use strict';
+/* eslint-disable no-unused-vars */
+const v1 = { value: 'Something' };
+const v2 = '';
+const v3 = undefined;
+const v4 = [3, 5, 7, 9];
+const v5 = {
+  name: 'Carl',
+  details: {
+    age: 82,
+  },
+};
---
+++ typescript-operators/main.ts
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+
+/* interface Customer {
+  name: string;
+  details?: {
+    address?: {
+      city: string;
+      street: string;
+      zip: string;
+    };
+    age?: number;
+  };
+}
+
+const v1 = { value: 'Something' };
+const v2 = '';
+const v3 = undefined;
+const v4 = [3, 5, 7, 9];
+const v5: Customer = {
+  name: 'Carl',
+  details: {
+    age: 82,
+  },
+}; */
