e9051d68363da4c7abd3f7e9cf820f83b9da11a4
Devin Olesen <devintanielu@gmail.com>
2024-10-11 03:36:27 +0000

    finish

--- typescript-promises/main.ts
+++ typescript-promises/main.ts
@@ -1,1 +1,4 @@
 import { takeAChance } from './take-a-chance.js';
+takeAChance('Devin')
+  .then((message: string) => console.log(message))
+  .catch((error: Error) => console.error(error.message));
