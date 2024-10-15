91f5aa3bef7eb5413a024d897009306b86fa764e
Devin Olesen <devintanielu@gmail.com>
2024-10-15 08:28:39 +0000

    async functions with fetch GET reqs

--- fetch/main.js
+++ fetch/main.js
@@ -1,44 +1,29 @@
-interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-}
-
-async function fetchUsers(): Promise<User[]> {
+'use strict';
+async function fetchUsers() {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    const data: User[] = await response.json();
+    const data = await response.json();
     console.log(data);
   } catch (error) {
     console.error('Error:', error);
   }
   return [];
 }
-
 fetchUsers();
-
-interface Pokemon {
-  name: string;
-  height: number;
-  weight: number;
-}
-
-async function fetchPokemon(): Promise<Pokemon[]> {
+async function fetchPokemon() {
   try {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    const data: Pokemon = await response.json();
+    const data = await response.json();
     console.log(data);
   } catch (error) {
     console.error('Error:', error);
   }
   return [];
 }
-
 fetchPokemon();
